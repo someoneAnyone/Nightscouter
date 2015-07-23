be5bf74c4838360f6e1dd8efdce8e7d2a61ba3bc
Peter Ina <peter.ina@gmail.com>
2015-07-23 16:45:31 -0400

    Adding new view for displaying sgv color coding

---
+++ Nightscouter/ColorBlockView.swift
@@ -0,0 +1,24 @@
+//
+//  colorBlockView.swift
+//  Nightscouter
+//
+//  Created by Peter Ina on 7/23/15.
+//  Copyright (c) 2015 Peter Ina. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+
+class ColorBlockView: UIView {
+
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    
+        NSAssetKit.drawColorBlockBackgroundView(arrowTintColor: self.backgroundColor!, backgroundFrame: rect)
+    }
+
+
+}
