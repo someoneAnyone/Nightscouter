10f5f64e757b390dab22dabd9550981014b17b0a
Peter Ina <peter.ina@gmail.com>
2015-08-04 18:23:23 -0400

    Moving stale view code to the compass class
    consolidated the tweaks needed to make the compass look stale.

--- Nightscouter/CompassControl+Convience.swift
+++ Nightscouter/CompassControl+Convience.swift
@@ -10,19 +10,31 @@ import UIKit
 
 extension CompassControl {
     
-    
-    internal func configure(sgvText: String, color: UIColor , sgvValue: Int, direction: Direction, date: NSDate, bgdelta: Int, units: String) -> Void {
+    internal func configure(sgvText: String, color: UIColor, direction: Direction, bgdelta: Int, units: String) -> Void {
         self.sgvText = sgvText
         self.color = color
         self.direction = direction
-        
         self.delta = "\(bgdelta.formattedForBGDelta) \(units)"
     }
     
     func configureWith(site: Site){
         if let configuration: ServerConfiguration = site.configuration,  watch: WatchEntry = site.watchEntry,  sgv: SensorGlucoseValue = watch.sgv {
             let color = colorForDesiredColorState(site.configuration!.boundedColorForGlucoseValue(sgv.sgv))
-            configure(sgv.sgvString, color: color, sgvValue: sgv.sgv, direction: sgv.direction, date: watch.date, bgdelta: watch.bgdelta, units: configuration.unitsRoot!.rawValue)
+            configure(sgv.sgvString, color: color, direction: sgv.direction, bgdelta: watch.bgdelta, units: configuration.unitsRoot!.rawValue)
         }
     }
+    
+    func shouldLookStale(look stale: Bool) {
+        if stale {
+            let compass = CompassControl()
+            self.alpha = 0.5
+            self.color = compass.color
+            self.sgvText = compass.sgvText
+            self.direction = compass.direction
+            self.delta = compass.delta
+        } else {
+            self.alpha = 1.0
+        }
+    }
+
 }
