30d2ac84942591025c632e171af336d3e60230c7
Peter Ina <peter.ina@gmail.com>
2015-08-11 08:51:37 -0400

    Revert "Revert 3df0341..d5a7c36"
    This reverts commit 325b3e4ef970e4a11bde9252b30858adf48bcea9.

---
+++ NightscouterNow/SiteNSNowTableViewCell.swift
@@ -0,0 +1,164 @@
+//
+//  SiteTableViewswift
+//  Nightscouter
+//
+//  Created by Peter Ina on 6/16/15.
+//  Copyright © 2015 Peter Ina. All rights reserved.
+//
+
+import UIKit
+import NightscouterKit
+
+class SiteNSNowTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var siteLastReadingHeader: UILabel!
+    @IBOutlet weak var siteLastReadingLabel: UILabel!
+    
+    @IBOutlet weak var siteBatteryHeader: UILabel!
+    @IBOutlet weak var siteBatteryLabel: UILabel!
+    
+    @IBOutlet weak var siteRawHeader: UILabel!
+    @IBOutlet weak var siteRawLabel: UILabel!
+    
+    @IBOutlet weak var siteNameLabel: UILabel!
+    
+    // @IBOutlet weak var siteColorBlockView: UIView!
+    // @IBOutlet weak var siteCompassControl: CompassControl!
+    
+    @IBOutlet weak var siteSgvLabel: UILabel!
+    @IBOutlet weak var siteDirectionLabel: UILabel!
+    
+//    @IBOutlet weak var siteDeltaHeader: UILabel!
+//    @IBOutlet weak var siteDeltaLabel: UILabel!
+    
+    // @IBOutlet weak var siteUrlLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.backgroundColor = UIColor.clearColor()
+    }
+    
+    func configureCell(site: Site) {
+        
+        // siteUrlLabel.text = site.url.host
+        
+        let defaultTextColor = Theme.Color.labelTextColor
+        
+        if let configuration = site.configuration {
+            
+            let maxValue: NSTimeInterval
+            if let defaults = configuration.defaults {
+                siteNameLabel.text = defaults.customTitle
+                maxValue = max(Constants.NotableTime.StaleDataTimeFrame, defaults.alarms.alarmTimeAgoWarnMins)
+            } else {
+                siteNameLabel.text = configuration.name
+                maxValue = Constants.NotableTime.StaleDataTimeFrame
+            }
+            
+            if let watchEntry = site.watchEntry {
+                // Configure compass control
+                // siteCompassControl.configureWith(site)
+                
+                // Battery label
+                siteBatteryLabel.text = watchEntry.batteryString
+                siteBatteryLabel.textColor = colorForDesiredColorState(watchEntry.batteryColorState)
+                
+                // Last reading label
+                siteLastReadingLabel.text = watchEntry.dateTimeAgoString
+                
+                if let sgvValue = watchEntry.sgv {
+                    
+                    let color = colorForDesiredColorState(site.configuration!.boundedColorForGlucoseValue(sgvValue.sgv))
+                    // siteColorBlockView.backgroundColor = color
+                    siteSgvLabel.text = sgvValue.sgvString
+                    siteSgvLabel.textColor = color
+                    
+                    siteDirectionLabel.text = sgvValue.direction.description
+                    siteDirectionLabel.textColor = color
+                    
+                    if let enabledOptions = configuration.enabledOptions {
+                        let rawEnabled =  contains(enabledOptions, EnabledOptions.rawbg)
+                        if rawEnabled {
+                            if let rawValue = watchEntry.raw {
+                                let color = colorForDesiredColorState(configuration.boundedColorForGlucoseValue(Int(rawValue)))
+                                siteRawLabel?.textColor = color
+                                siteRawLabel.text = "\(NSNumberFormatter.localizedStringFromNumber(rawValue, numberStyle: .DecimalStyle)) : \(sgvValue.noise)"
+                            }
+                        } else {
+                            siteRawHeader.hidden = true
+                            siteRawLabel.hidden = true
+                        }
+                    }
+                    
+                    let timeAgo = watchEntry.date.timeIntervalSinceNow
+                    let isStaleData = configuration.isDataStaleWith(interval: timeAgo)
+                    // siteCompassControl.shouldLookStale(look: isStaleData.warn)
+                    
+                    if isStaleData.warn {
+                        siteBatteryLabel?.text = "---%"
+                        siteBatteryLabel?.textColor = defaultTextColor
+                        siteRawLabel?.text = "--- : ---"
+                        siteRawLabel?.textColor = defaultTextColor
+                        siteLastReadingLabel?.textColor = NSAssetKit.predefinedWarningColor
+                        // siteColorBlockView.backgroundColor = colorForDesiredColorState(DesiredColorState.Neutral)
+                        
+                        siteSgvLabel.text = "---"
+                        siteSgvLabel.textColor = colorForDesiredColorState(.Neutral)
+                        
+                        siteDirectionLabel.text = "----"
+                        siteDirectionLabel.textColor = colorForDesiredColorState(.Neutral)
+                    }
+                    
+                    if isStaleData.urgent{
+                        siteLastReadingLabel?.textColor = NSAssetKit.predefinedAlertColor
+                    }
+                    
+                } else {
+                    #if DEBUG
+                        println("No SGV was found in the watch")
+                    #endif
+                }
+                
+            } else {
+                // No watch was there...
+                #if DEBUG
+                    println("No watch data was found...")
+                #endif
+            }
+        } else {
+            #if DEBUG
+                println("No site current configuration was found for \(site.url)")
+            #endif
+        }
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        siteNameLabel.text = nil
+        siteBatteryLabel.text = nil
+        siteRawLabel.text = nil
+        siteLastReadingLabel.text = nil
+        // siteCompassControl.shouldLookStale(look: true)
+        // siteColorBlockView.backgroundColor = siteCompassControl.color
+        
+        siteSgvLabel.text = nil
+        siteSgvLabel.textColor = Theme.Color.labelTextColor
+        
+        siteDirectionLabel.text = nil
+        siteDirectionLabel.textColor = Theme.Color.labelTextColor
+        
+        siteLastReadingLabel.text = Constants.LocalizedString.tableViewCellLoading.localized
+        siteLastReadingLabel.textColor = Theme.Color.labelTextColor
+        
+        siteRawHeader.hidden = false
+        siteRawLabel.hidden = false
+        siteRawLabel.textColor = Theme.Color.labelTextColor
+    }
+}
---
+++ NightscouterNow/TodayViewController.swift
@@ -0,0 +1,194 @@
+//
+//  TodayViewController.swift
+//  NightscouterNow
+//
+//  Created by Peter Ina on 8/8/15.
+//  Copyright (c) 2015 Peter Ina. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+import NightscouterKit
+
+class TodayViewController: UITableViewController, NCWidgetProviding {
+    
+    var sites:[Site] {
+        return AppDataManager.sharedInstance.sites
+    }
+    
+    // Whenever this changes, it updates the attributed title of the refresh control.
+    var lastUpdatedTime: NSDate? {
+        didSet{
+            // Create and use a formatter.
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.timeStyle = NSDateFormatterStyle.MediumStyle
+            dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+            dateFormatter.timeZone = NSTimeZone.localTimeZone()
+            
+            if let date = lastUpdatedTime {
+                let str = String(stringInterpolation:Constants.LocalizedString.lastUpdatedDateLabel.localized, dateFormatter.stringFromDate(date))
+            }
+        }
+    }
+
+    let expandButton = UIButton()
+    
+    let userDefaults = AppDataManager.sharedInstance.defaults
+    
+    var expanded : Bool {
+        get {
+            return userDefaults.boolForKey("expanded")
+        }
+        set (newExpanded) {
+            userDefaults.setBool(newExpanded, forKey: "expanded")
+            userDefaults.synchronize()
+        }
+    }
+    
+    let defaultNumRows = 3
+    let maxNumberOfRows = 6
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+        
+        updateExpandButtonTitle()
+        expandButton.addTarget(self, action: "toggleExpand", forControlEvents: .TouchUpInside)
+
+        tableView.estimatedRowHeight = 80
+        tableView.sectionFooterHeight = 44
+        
+        updatePreferredContentSize()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        tableView.reloadData()
+        completionHandler(NCUpdateResult.NewData)
+    }
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+    func updatePreferredContentSize() {
+        preferredContentSize = tableView.contentSize
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        coordinator.animateAlongsideTransition({ context in
+            self.tableView.frame = CGRectMake(0, 0, size.width, size.height)
+            }, completion: nil)
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !sites.isEmpty {
+            return min(sites.count, expanded ? maxNumberOfRows : defaultNumRows)
+        }
+        return 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("nsSiteNow", forIndexPath: indexPath) as! SiteNSNowTableViewCell
+        
+        let site = sites[indexPath.row]
+        cell.configureCell(site)
+        
+        if (lastUpdatedTime?.timeIntervalSinceNow > 60 || lastUpdatedTime == nil || site.configuration == nil) {
+            // No configuration was there... go get some.
+            // println("Attempting to get configuration data from site...")
+            loadDataFor(site, index: indexPath.row)
+        }
+
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return expandButton
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let item = sites[indexPath.row]
+        
+        if let context = extensionContext {
+
+            AppDataManager.sharedInstance.currentSiteIndex = indexPath.row
+            let url = NSURL(string: "nightscouter://link/\(Constants.StoryboardViewControllerIdentifier.SiteListPageViewController.rawValue)")
+            context.openURL(url!, completionHandler: nil)
+        }
+
+    }
+
+    
+    // MARK: expand
+    
+    func updateExpandButtonTitle() {
+        expandButton.setTitle(expanded ? "Show less" : "Show more", forState: .Normal)
+    }
+    
+    func toggleExpand() {
+        expanded = !expanded
+        updateExpandButtonTitle()
+        updatePreferredContentSize()
+        tableView.reloadData()
+    }
+    
+    func updateData(){
+        // Do not allow refreshing to happen if there is no data in the sites array.
+        if sites.isEmpty == false {
+            for site in sites {
+                loadDataFor(site, index: find(sites, site)!)
+            }
+        } else {
+            // No data in the sites array. Cancel the refreshing!
+        }
+    }
+    
+    func loadDataFor(site: Site, index: Int){
+        // Start up the API
+        let nsApi = NightscoutAPIClient(url: site.url)
+        
+        //TODO: 1. There should be reachabiltiy checks before doing anything.
+        //TODO: 2. We should fail gracefully if things go wrong. Need to present a UI for reporting errors.
+        //TODO: 3. Probably need to move this code to the application delegate?
+        
+        // Get settings for a given site.
+        println("Loading data for \(site.url!)")
+        nsApi.fetchServerConfiguration { (result) -> Void in
+            switch (result) {
+            case let .Error(error):
+                // display error message
+                 println("loadUpData ERROR recieved: \(error)")                
+            case let .Value(boxedConfiguration):
+                let configuration:ServerConfiguration = boxedConfiguration.value
+                // do something with user
+                nsApi.fetchDataForWatchEntry({ (watchEntry, watchEntryErrorCode) -> Void in
+                    // Get back on the main queue to update the user interface
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        site.configuration = configuration
+                        site.watchEntry = watchEntry
+                        AppDataManager.sharedInstance.updateSite(site)
+                        self.lastUpdatedTime = NSDate()
+
+                        
+                        self.tableView.reloadRowsAtIndexPaths([NSIndexPath(forRow: index, inSection: 0)], withRowAnimation: .Automatic)
+                    })
+                })
+            }
+        }
+
+    }
+}
