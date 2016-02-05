3130b54128a9108ec1213af2c361a5bbb167c013
Peter Ina <peter.ina@gmail.com>
2016-02-04 21:36:20 -0500

    refactored timeline generation to common spot.

--- Shared Framework Code/API/NightscoutAppDataManager.swift
+++ Shared Framework Code/API/NightscoutAppDataManager.swift
@@ -8,45 +8,109 @@
 
 import Foundation
 
-//public protocol ApplicationDataManageable {
-//
-//    var defaults: NSUserDefaults { get }
-//
-//    var models: [WatchModel] { get }
-//
-//    var sites: [Site] { get }
-//
-//    var currentSiteIndex: Int { get set }
-//
-//    var shouldDisableIdleTimer: Bool { get set }
-//
-//    func addSite(site: Site, index: Int?)
-//
-//    func updateSite(site: Site) -> Bool
-//
-//    func deleteSiteAtIndex(index: Int)
-//
-//    func loadSampleSites()
-//
-//
-//    func loadDataFor(site: Site, index: Int?, withChart: Bool?, completetion: (returnedModel: WatchModel?, returnedSite: Site?, returnedIndex: Int?, returnedError: NSError?) -> Void)
-//}
-//
-//
-//extension ApplicationDataManageable {
-//
-//    var NightscouterGroup: String {
-//        return "group.com.nothingonline.nightscouter"
-//}
-//
-//
-//    public var defaults: NSUserDefaults {
-//     return NSUserDefaults(suiteName: NightscouterGroup)!
-//    }
-//
-//
-//}
 
+public func generateComplicationModels(forSite site: Site, calibrations: [Calibration]) -> [ComplicationModel] {
+    
+    let cals = calibrations.sort{(item1: Calibration, item2: Calibration) -> Bool in
+        item1.date.compare(item2.date) == NSComparisonResult.OrderedDescending
+        }
+    
+    guard let configuration = site.configuration, displayName = site.configuration?.displayName, entries = site.entries else {
+        return []
+    }
+    
+    var cmodels: [ComplicationModel] = []
+    
+    // Get prefered Units. mmol/L or mg/dL
+    let units: Units = configuration.displayUnits
+    
+    for (index, entry) in entries.enumerate() {
+        
+        if let sgvValue = entry.sgv {
+            
+            // Convert units.
+            var boundedColor = configuration.boundedColorForGlucoseValue(sgvValue.sgv)
+            if units == .Mmol {
+                boundedColor = configuration.boundedColorForGlucoseValue(sgvValue.sgv)
+            }
+            
+            
+            var sgvString = "\(sgvValue.sgv.formattedForMgdl)"
+            if configuration.displayUnits == .Mmol {
+                sgvString = sgvValue.sgv.formattedForMmol
+            }
+            
+            sgvString =  "\(sgvValue.sgvString(forUnits: units))"
+            let sgvEmoji = "\(sgvValue.direction.emojiForDirection)"
+            let sgvStringWithEmoji = "\(sgvString) \(sgvValue.direction.emojiForDirection)"
+            
+            var delta: Double = 0
+            
+            let nextIndex: Int = index + 1
+            
+            if nextIndex < entries.count {
+                if let previousSgv = entries[nextIndex].sgv {
+                    if sgvValue.isSGVOk && previousSgv.isSGVOk {
+                        delta = sgvValue.sgv - previousSgv.sgv
+                    }
+                }
+            }
+            
+            if configuration.displayUnits == .Mmol {
+                delta = delta.toMmol
+            }
+            
+            let deltaString = delta.formattedBGDelta(forUnits: units)
+            let deltaStringShort = delta.formattedBGDelta(forUnits: units, appendString: "∆")
+            let sgvColor = colorForDesiredColorState(boundedColor)
+            
+            var raw = ""
+            var rawShort = ""
+            
+            if let cal = nearestCalibration(calibrations: cals, calibrationsforDate: entry.date) {
+                
+                var convertedRawValue: String = sgvValue.rawIsigToRawBg(cal).formattedForMgdl
+                if configuration.displayUnits == .Mmol {
+                    convertedRawValue = sgvValue.rawIsigToRawBg(cal).formattedForMmol
+                }
+                
+                raw = "\(convertedRawValue) : \(sgvValue.noise.description)"
+                rawShort = "\(convertedRawValue) : \(sgvValue.noise.description[sgvValue.noise.description.startIndex])"
+            }
+            
+            let model = ComplicationModel(displayName: displayName, date: entry.date, sgv: sgvStringWithEmoji, sgvEmoji: sgvEmoji, tintString: sgvColor.toHexString(), delta: deltaString, deltaShort: deltaStringShort, raw: raw, rawShort: rawShort)
+            
+            cmodels.append( model)
+            
+        }
+        
+    }
+    return cmodels
+}
+
+
+public func nearestCalibration(calibrations cals:[Calibration], calibrationsforDate date: NSDate) -> Calibration? {
+        var desiredIndex: Int?
+        var minDate: NSTimeInterval = fabs(NSDate().timeIntervalSinceNow)
+    
+        for (index, entry) in cals.enumerate() {
+            let dateInterval = fabs(entry.date.timeIntervalSinceDate(date))
+            let compared = minDate < dateInterval
+            // print("Testing: \(minDate) < \(dateInterval) = \(compared)")
+            if compared {
+                minDate = dateInterval
+                desiredIndex = index
+            }
+        }
+        
+        guard let index = desiredIndex else {
+            print("no valid index was found... return last calibration")
+            return cals.first
+        }
+        
+        // print("incoming date: \(closestDate.timeIntervalSinceNow) returning date: \(calibrations[index].date.timeIntervalSinceNow)")
+        return cals[index]
+    }
 
 
 public func loadDataFor(model: WatchModel, replyHandler:(model: WatchModel) -> Void) {
@@ -96,12 +160,11 @@ public func loadDataFor(site: Site, index: Int?, withChart: Bool = false, comple
                 
                 if !withChart {
                     dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                        //                            self.updateSite(site)
                         completetion(returnedModel: WatchModel(fromSite: site), returnedSite: site, returnedIndex: index, returnedError: nil)
                     })
                 } else {
                     
-                    nsApi.fetchDataForEntries(Constants.EntryCount.NumberForChart) { (entries, errorCode) -> Void in
+                    nsApi.fetchDataForEntries(Constants.EntryCount.NumberForComplication) { (entries, errorCode) -> Void in
                         if let entries = entries {
                             site.entries = entries
                             
