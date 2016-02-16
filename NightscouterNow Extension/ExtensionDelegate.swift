6ff0fda120c4e0b6f9cb252ccf2cc141e19b9d10
Peter Ina <peter.ina@gmail.com>
2016-02-16 18:29:07 -0500

    extension update

--- NightscouterNow Extension/ExtensionDelegate.swift
+++ NightscouterNow Extension/ExtensionDelegate.swift
@@ -12,10 +12,6 @@ import NightscouterWatchOSKit
 class ExtensionDelegate: NSObject, WKExtensionDelegate {
     var timer: NSTimer?
     
-    override init() {
-        WatchSessionManager.sharedManager.startSession()
-    }
-
     func applicationDidFinishLaunching() {
         #if DEBUG
             print(">>> Entering \(__FUNCTION__) <<<")
@@ -23,8 +19,7 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         
         // Perform any final initialization of your application.
         
-        
-        //WatchSessionManager.sharedManager.startSession()
+        WatchSessionManager.sharedManager.startSession()
         updateDataNotification(nil)
     }
     
@@ -32,17 +27,14 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
         #if DEBUG
             print(">>> Entering \(__FUNCTION__) <<<")
         #endif
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-
     }
  
     func applicationWillResignActive() {
         #if DEBUG
             print(">>> Entering \(__FUNCTION__) <<<")
         #endif
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, etc.
-        WatchSessionManager.sharedManager.endSession()
+     
+        WatchSessionManager.sharedManager.saveData()
         self.timer?.invalidate()
     
     }
@@ -55,11 +47,13 @@ class ExtensionDelegate: NSObject, WKExtensionDelegate {
     func updateDataNotification(timer: NSTimer?) -> Void {
         #if DEBUG
             print(">>> Entering \(__FUNCTION__) <<<")
-            print("Posting \(NightscoutAPIClientNotification.DataIsStaleUpdateNow) Notification at \(NSDate())")
+            print("ExtensionDelegate:   Posting \(NightscoutAPIClientNotification.DataIsStaleUpdateNow) notification at \(NSDate())")
         #endif
         
         dispatch_async(dispatch_get_main_queue()) {
             NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: NightscoutAPIClientNotification.DataIsStaleUpdateNow, object: self))
+            ComplicationController.reloadComplications()
+            WatchSessionManager.sharedManager.saveData()
         }
         
         if (self.timer == nil) {
