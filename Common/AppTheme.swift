383f2c7e22a035ba1e2dab322daaa801da188ed1
Peter Ina <peter.ina@gmail.com>
2016-09-22 21:19:16 -0400

    Fixing the theme colors.

--- Common/AppTheme.swift
+++ Common/AppTheme.swift
@@ -10,11 +10,10 @@ import UIKit
 
 public struct Theme {
     public struct AppColor {
-        public static let windowTintColor = Color(red: 0.851, green: 0.851, blue: 0.851, alpha: 1.000)// NightscouterAssetKit.predefinedNeutralColor
+        public static let windowTintColor = NSAssetKit.predefinedNeutralColor
         public static let headerTextColor = Color.white.withAlphaComponent(0.5)
         public static let labelTextColor = Color.white
-        public static let navBarColor: Color = Color(red: 0.000, green: 0.451, blue: 0.812, alpha: 1.000)
-//NightscouterAssetKit.darkNavColor
+        public static let navBarColor: Color = NSAssetKit.darkNavColor
         public static let navBarTextColor: Color = Color.white
     }
     
@@ -28,15 +27,12 @@ public struct Theme {
         window?.tintColor = Theme.AppColor.windowTintColor
         
         if let navBarFont = Theme.Font.navBarTitleFont {
-            
-            let navBarColor: UIColor = Theme.AppColor.navBarColor
+            let navBarColor: Color = Theme.AppColor.navBarColor
             UINavigationBar.appearance().barTintColor = navBarColor
             UINavigationBar.appearance().tintColor = Theme.AppColor.windowTintColor
             
-            let navBarAttributesDictionary: [String: AnyObject]? = [
-                NSForegroundColorAttributeName: Theme.AppColor.navBarTextColor,
-                NSFontAttributeName: navBarFont
-            ]
+            let navBarAttributesDictionary: [String: Any]? = [ NSForegroundColorAttributeName: Theme.AppColor.navBarTextColor,
+                NSFontAttributeName: navBarFont]
             
             UINavigationBar.appearance().titleTextAttributes = navBarAttributesDictionary
         }
